2ca0f8e718adc5380375d0d933ccc50d88be400f
neelb99 <daveescott0509@gmail.com>
2024-07-01 00:00:09 -0500

    work experience updated
    Co-Authored-By: OnlyForward0613 <153643788+Onlyforward0613@users.noreply.github.com>

--- frontend/content/Experience.ts
+++ frontend/content/Experience.ts
@@ -1,8 +1,8 @@
 const Experience = [
     {
         id: 1,
-        slug: "111",
-        company: "Ruby Exchange",
+        slug: "RUBY Exchange",
+        company: "RUBY Exchange",
         company_image: "/images/companies/rubyexchange.svg",
         company_url: "https://ruby.exchange/",
         address: "Flughafen, Zürich, Switzerland",
@@ -12,7 +12,7 @@ const Experience = [
         start_date: "Nov 2021",
         end_date: "Feb 2023",
         duration: "Nov 2021 - Feb 2023",
-        duration_in_days: "(1 year, 3 months)",
+        duration_in_days: "1 year, 3 months",
         currently_working: "string",
         description: `<div class="relative md:border-gray-300 md:dark:border-gray-400 md:col-span-4 text-sm text-inherit font-light pt-6"><p class="MsoListParagraphCxSpFirst" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora; font-weight: bold; font-size: 16px; margin-bottom: 5px">DEX platform for crypto trading on Ethergem blockchain.</span></p>
         <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Developed a web UI of DEX platform for crypto trading using Next.JS.</span></p>
@@ -23,63 +23,87 @@ const Experience = [
         created_at: "string",
         updated_at: "string",
     },
-    // {
-    //     id: 2,
-    //     slug: "string",
-    //     company: "string",
-    //     company_image: "string",
-    //     company_url: "string",
-    //     address: "string",
-    //     designation: "string",
-    //     job_type: "string",
-    //     job_location_type: "string",
-    //     start_date: "string",
-    //     end_date: "string",
-    //     duration: "string",
-    //     duration_in_days: "string",
-    //     currently_working: "string",
-    //     description: "string",
-    //     created_at: "string",
-    //     updated_at: "string",
-    // },
-    // {
-    //     id: 3,
-    //     slug: "string",
-    //     company: "string",
-    //     company_image: "string",
-    //     company_url: "string",
-    //     address: "string",
-    //     designation: "string",
-    //     job_type: "string",
-    //     job_location_type: "string",
-    //     start_date: "string",
-    //     end_date: "string",
-    //     duration: "string",
-    //     duration_in_days: "string",
-    //     currently_working: "string",
-    //     description: "string",
-    //     created_at: "string",
-    //     updated_at: "string",
-    // },
-    // {
-    //     id: 4,
-    //     slug: "string",
-    //     company: "string",
-    //     company_image: "string",
-    //     company_url: "string",
-    //     address: "string",
-    //     designation: "string",
-    //     job_type: "string",
-    //     job_location_type: "string",
-    //     start_date: "string",
-    //     end_date: "string",
-    //     duration: "string",
-    //     duration_in_days: "string",
-    //     currently_working: "string",
-    //     description: "string",
-    //     created_at: "string",
-    //     updated_at: "string",
-    // }
+    {
+        id: 2,
+        slug: "MonoX",
+        company: "MonoX",
+        company_image: "/images/companies/monoX.png",
+        company_url: "https://monox.finance/home",
+        address: "Marlborough Hill Harrow, London, United Kingdom",
+        designation: "Lead Smart Contract Developer",
+        job_type: "Full Time",
+        job_location_type: "Remote",
+        start_date: "May 2020",
+        end_date: "Oct 2021",
+        duration: "May 2020 - Oct 2021",
+        duration_in_days: "1 year, 5 months",
+        currently_working: "string",
+        description: `<div class="relative md:border-gray-300 md:dark:border-gray-400 md:col-span-4 text-sm text-inherit font-light pt-6"><p class="MsoListParagraphCxSpFirst" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora; font-weight: bold; font-size: 16px; margin-bottom: 5px">The objective of MonoX Protocol is to create a new design for capital inefficiencies
+        caused by liquidity pairs, by using a single token pool model.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Designed the smart contract structure and broke down based on the functions.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Developed the smart contracts by solidity and tested using hardhat.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Improved the security of the smart contracts with audit.</span></p></div>`,
+        created_at: "string",
+        updated_at: "string",
+    },
+    {
+        id: 3,
+        slug: "Bidify Org",
+        company: "Bidify Org",
+        company_image: "/images/companies/bidifyOrg.svg",
+        company_url: "https://bidify.org/",
+        address: "Bedford, England",
+        designation: "Blockchain Full Stack Developer",
+        job_type: "Full Time",
+        job_location_type: "Remote",
+        start_date: "Nov 2018",
+        end_date: "Apr 2020",
+        duration: "Nov 2018 - Apr 2020",
+        duration_in_days: "1 year, 5 months",
+        currently_working: "string",
+        description: `<div class="relative md:border-gray-300 md:dark:border-gray-400 md:col-span-4 text-sm text-inherit font-light pt-6"><p class="MsoListParagraphCxSpFirst" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora; font-weight: bold; font-size: 16px; margin-bottom: 5px">NFT Marketplace on several blockchains.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Built the smart contracts by solidity to buy, sell and auction NFTs on the
+        platform.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Developed robust backend services using Node.js and Express.js, handling
+        server-side logic, database interactions, and API endpoints for the NFT
+        marketplace</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Developing a scalable and high-performance backend architecture to support
+        the growing demands of the platform.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Took part in the development of UI using ReactJS.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Conceived and implemented a new user interface for a custom approval
+        processes.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Integrated with 3rd party APIs such as IPFS, Arweave and Fleek.</span></p>
+        </div>`,
+        created_at: "string",
+        updated_at: "string",
+    },
+    {
+        id: 5,
+        slug: "XION Finance",
+        company: "XION Finance",
+        company_image: "https://www.xion.finance/img/XFlogoLarge.svg",
+        company_url: "https://www.xion.finance/",
+        address: "Marlborough Hill Harrow, London, United Kingdom",
+        designation: "Lead Smart Contract Developer",
+        job_type: "Full Time",
+        job_location_type: "Remote",
+        start_date: "May 2020",
+        end_date: "Oct 2021",
+        duration: "May 2020 - Oct 2021",
+        duration_in_days: "1 year, 5 months",
+        currently_working: "string",
+        description: `<div class="relative md:border-gray-300 md:dark:border-gray-400 md:col-span-4 text-sm text-inherit font-light pt-6"><p class="MsoListParagraphCxSpFirst" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora; font-weight: bold; font-size: 16px; margin-bottom: 5px">DeFi Xion Global cross-chain Staking - crypto shopping up to 100% cashback
+        rewards</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Designed and implemented the backend architecture for the platform, focusing
+        on scalability, reliability, and performance.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Managed the database layer, optimizing queries for performance and ensuring
+        data integrity. Worked closely with the frontend team to ensure real-time data
+        synchronization and user experience optimization.</span></p>
+        <p class="MsoListParagraphCxSpMiddle" style="text-indent: -.25in; mso-list: l0 level1 lfo1;"><!-- [if !supportLists]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;"><span style="mso-list: Ignore;">●<span style="font: 7.0pt 'Times New Roman';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></span></span><!--[endif]--><span lang="EN" style="font-family: Lora; mso-fareast-font-family: Lora; mso-bidi-font-family: Lora;">Conducted regular security audits of the smart contracts and backend
+        infrastructure to identify and mitigate potential vulnerabilities.</span></p></div>`,
+        created_at: "string",
+        updated_at: "string",
+    },
 ];
 
 export default Experience;
