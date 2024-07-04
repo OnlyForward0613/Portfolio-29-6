65a00e99a17492190a96790492fc27e5f208a47e
neelb99 <daveescott0509@gmail.com>
2024-07-04 00:18:03 -0500

    Update Certificates.ts

--- frontend/content/Certificates.ts
+++ frontend/content/Certificates.ts
@@ -10,6 +10,18 @@ const Certificate = [
         credential_id: "F4E6C43ECC3E",
         created_at: "3 Jul 2024",
         updated_at: "3 Jul 2024",
+    },
+    {
+        id: "2",
+        title: "HackerRank Certified JavaScript (Basic)",
+        slug: "HackerRank Certified JavaScript (Basic)",
+        organization: "HackerRank",
+        image: '/images/Certificates/Javascript.png',
+        issue_date: "20 Sep 2013",
+        expiration_date: "Does not expire",
+        credential_id: "24147172CA87",
+        created_at: "3 Jul 2024",
+        updated_at: "3 Jul 2024",
     }
 ];
 
