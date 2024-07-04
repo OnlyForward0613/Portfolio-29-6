49a8b0e993d0022a22e7c04bda9a400bfef722a8
neelb99 <daveescott0509@gmail.com>
2024-07-04 01:13:05 -0500

    interest section created

---
+++ frontend/content/Interest.ts
@@ -0,0 +1,13 @@
+const Interest = [
+    {
+        id: 1,
+        slug: "Trading",
+        title: "Trading",
+        icon: '',
+        order: 10,
+        created_at: "3 Jul 2024",
+        updated_at: "3 Jul 2024",
+    },
+];
+
+export default Interest;
