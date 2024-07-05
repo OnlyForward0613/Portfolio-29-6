e67ef39eb854209edd2afd6e5fd8e6c515ef13e2
neelb99 <daveescott0509@gmail.com>
2024-07-05 00:57:13 -0500

    updated project section

--- frontend/content/Project.ts
+++ frontend/content/Project.ts
@@ -13,7 +13,22 @@ const Project = [
         description: "Ruby.Exchange is a decentralized exchange (DEX) built as an Automated Market Maker (AMM) powered by NFT's for a gamified user experience",
         created_at: "05 Jul 2024",
         updated_at: "05 Jul 2024",
-    }
+    },
+    {
+        id: "2",
+        title: "Bidify Org",
+        slug: "Bidify Org",
+        image: '/images/Projects/BidifyOrg2.PNG',
+        short_description: "NFT Auctions. Bidify aims to be the premier tool for Auction listing and selling on the Ethereum Network.",
+        technology: "Blockchain, NFT MarketPlace, Auction Listing, Ethereum Network, ERC721",
+        duration: "Nov 2018 ~ Apr 2020",
+        duration_in_days: "1 Year 5 Months",
+        preview_url: "https://bidify.org/",
+        github_url: "https://github.com/OnlyForward0613/Bidify-Org-FE",
+        description: "NFT Auctions. Bidify aims to be the premier tool for Auction listing and selling on the Ethereum Network.",
+        created_at: "05 Jul 2024",
+        updated_at: "05 Jul 2024"
+    },
 ];
 
 export default Project;
