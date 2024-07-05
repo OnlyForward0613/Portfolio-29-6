14a1749a1df2638585ca8ecf31bed6f5ced1d151
neelb99 <daveescott0509@gmail.com>
2024-07-05 01:20:39 -0500

    code snippset part created

---
+++ frontend/content/CodeSnipset.ts
@@ -0,0 +1,34 @@
+const CodeSnipsets = [
+    {
+        slug: "Simple NFT Marketplace",
+        title: "Simple NFT Marketplace",
+        overview: "This Solidity implementation represents a decentralized approach to managing content, leveraging blockchain technology for transparency, immutability, and security. Each content item is stored as a struct within a smart contract, with attributes mirroring those of the Express implementation.",
+        image: "/images/snipset.png",
+        language: "Solidity, TypeScript",
+        tags: "NFT Marketplace, Smart Contract",
+        content: "Welcome to our cutting-edge NFT marketplace, a pioneering platform designed to revolutionize the way digital art and collectibles are bought, sold, and traded. Built atop the robust infrastructure of blockchain technology, our marketplace offers a secure, transparent, and efficient environment for both novice and seasoned collectors alike.",
+        order: 10,
+        total_views: 121,
+        total_likes: 32,
+        user_liked: true,
+        created_at: "5 Jul 2024",
+        updated_at: "5 Jul 2024"
+    },
+    {
+        slug: "Simple NFT Marketplace",
+        title: "Simple NFT Marketplace",
+        overview: "The project described above is a simple yet powerful Ethereum-based smart contract designed for swapping ERC20 tokens. This contract facilitates the exchange of one type of ERC20 token (Token A) for another (Token B) based on a predefined exchange rate.",
+        image: "/images/snipset.png",
+        language: "Solidity, TypeScript",
+        tags: "Exchange, SWAP, Ethereum, Smart Contract",
+        content: "Our project introduces a smart contract on the Ethereum blockchain that enables users to swap one ERC20 token (Token A) for another (Token B) at a fixed exchange rate. This contract is particularly useful in scenarios where users hold tokens that they wish to convert into another asset for various reasons, such as participating in different DeFi protocols, accessing new investment opportunities, or simply diversifying their holdings.",
+        order: 10,
+        total_views: 205,
+        total_likes: 46,
+        user_liked: true,
+        created_at: "5 Jul 2024",
+        updated_at: "5 Jul 2024"
+    }
+];
+
+export default CodeSnipsets;
